d01e01bb86a56d61261f2cb80cf31e2389155818
Dmoeen <daniyal.moeen@gmail.com>
2019-09-09 16:54:16 +0500

    this is commit of vuechange branch

--- Modules/User/Assets/js/components/CustomComponent.vue
+++ Modules/User/Assets/js/components/CustomComponent.vue
@@ -1,13 +1,15 @@
 <template>
     <h1>
        {{text}}
+       {{textnew}}
     </h1>
 </template>
 <script>
     export default {
         data(){
             return{
-                text:"Yepi I have figured out how to create vue components in laravel asgard"
+                text:"Yepi I have figured out how to create vue components in laravel asgard",
+                textnew:"every module has seperate Vue Moduling system which is very great and powerfull."
             }
         }
     }
