9e30f69e8d81c65e22220dd178e513c1b7b1c18f
Dmoeen <daniyal.moeen@gmail.com>
2019-09-09 16:17:15 +0500

    this directory is now clear

---
+++ Modules/User/Assets/js/components/CustomComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>
+       {{text}}
+    </h1>
+</template>
+<script>
+    export default {
+        data(){
+            return{
+                text:"Yepi I have figured out how to create vue components in laravel asgard"
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
---
+++ Modules/User/Resources/views/admin/users/custom.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content-header')
+@stop
+
+@section('content')
+@stop
+
+@section('footer')
+    <a data-toggle="modal" data-target="#keyboardShortcutsModal"><i class="fa fa-keyboard-o"></i></a> &nbsp;
+@stop
+@section('shortcuts')
+
+@stop
